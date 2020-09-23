5b7179981185c8e284f34f9ca657c41f8b3a0fc3
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 17:59:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect do |calls|
+    calls.capitalize + "!"
 end
 
 def long_planeteer_calls# code an argument here
