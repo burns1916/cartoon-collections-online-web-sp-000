e06bb889f9de1577b336d32ebba1b28d22561b7e
Ryan K Burns <ryankburns@gmail.com>
2020-09-23 18:50:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,4 +23,8 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.include?(cheese_types)
+    if true
+      return cheese_types[i]
+    else
+      return nil
 end
